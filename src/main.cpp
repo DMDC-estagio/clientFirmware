39ab3b4b44fc2b3a2c00f6f6665349e4d1999311
gab5987 <biell.gb4@gmail.com>
2022-07-25 20:58:23 -0400

    FEAT. Add measurement functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 #include "helper.h"
 
+
+unsigned int measureCount = 0;
+
 void setup() {
+  // Since arduino cant handle Wifi nor HTTP, those lines are not necesary 
   #ifndef devBoard arduino
     initWifi();
     initMQTT();
@@ -20,14 +24,30 @@ void loop() {
       }
     #endif
 
-    if(millis() - lastMillis >= 0x15F90) {
+    if(millis() - lastMillis >= 0x15F90) { // Waits 1'30" until the next http request
+      voltage.med = voltage.med / measureCount;
+      current.med = current.med / measureCount;
       sendHttp(voltage, current);
       lastMillis = millis();
+
+      measureCount = 0; 
+      current.max = 0; current.min = 1000;
+      voltage.max = 0; voltage.min = 1000; // Setting minimum values to 1000 garants that it wont receive a faulty value
     }
+    // Measure voltage and current
     float measuredCurrent = measureCurrent();
     float measuredVoltage = measureVoltage();
 
-    voltage.med = (voltage.med + measuredVoltage) / 2;
+    voltage.med += measuredVoltage;
+    current.med += measuredCurrent;
+    measureCount++;
+
+    if(measuredCurrent > current.max) { current.max = measuredCurrent; }
     if(measuredVoltage > voltage.max) { voltage.max = measuredVoltage; }
+
+    if(measuredCurrent < current.min) { current.min = measuredCurrent; }
+    if(measuredVoltage < voltage.min) { voltage.min = measuredVoltage; }
+
+    delayMicroseconds(100);
   }
 }
